3810c33e4470a7693519b7b05793ecffc4695fe3
CV2 CV2 <2cv36985@gmail.com>
2024-05-25 18:29:59 -0500

    Empezando develop

---
+++ src/HolaMundo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HolaMundo() {
+  return (
+    <div>
+      <h2>Hola Mundo!</h2>
+      <p>Este es un componente de prueba.</p>
+    </div>
+  );
+}
+
+export default HolaMundo;
